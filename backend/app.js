e0c2567b04855c007fffde17a8011d69a84ef8f3
ertan3 <ernesttan96@gmail.com>
2023-07-02 23:33:41 -0700

    Compelete express routing

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/api/users');
+const tweetsRouter = require('./routes/api/tweets');
 
 const app = express();
 
@@ -12,9 +11,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/tweets', tweetsRouter);
 
 module.exports = app;
