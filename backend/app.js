b4446b2addc0365e9c8def5eab55df8ccf62db41
ertan3 <ernesttan96@gmail.com>
2023-07-02 23:46:01 -0700

    Complete express error handling

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const { isProduction } = require('./config/keys');
 
-const usersRouter = require('./routes/api/users');
-const tweetsRouter = require('./routes/api/tweets');
-const csrfRouter = require('./routes/api/csrf');
+
 
 const app = express();
 
@@ -17,7 +15,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// ADD THIS SECURITY MIDDLEWARE
 // Security Middleware
 if (!isProduction) {
     // Enable CORS only in development because React will be on the React
@@ -38,8 +35,35 @@ app.use(
 );
 
 // Attach Express routers
+const usersRouter = require('./routes/api/users');
+const tweetsRouter = require('./routes/api/tweets');
+const csrfRouter = require('./routes/api/csrf');
 app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetsRouter);
 app.use('/api/csrf', csrfRouter);
+
+// Express custom middleware for catching all unmatched requests and formatting
+// a 404 error to be sent as the response.
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+});
+
+const serverErrorLogger = debug('backend:error');
+
+  // Express custom error handler that will be called whenever a route handler or
+  // middleware throws an error or invokes the `next` function with a truthy value
+app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+        message: err.message,
+        statusCode,
+        errors: err.errors
+    })
+});
+
 
 module.exports = app;
