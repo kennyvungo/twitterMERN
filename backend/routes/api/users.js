f983e2ea0aa4e80e0a8636bddbfd92c45b25e255
ertan3 <ernesttan96@gmail.com>
2023-07-03 00:15:43 -0700

    Complete validations

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -5,8 +5,11 @@ const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const passport = require('passport');
 const { loginUser, restoreUser } = require('../../config/passport');
-const { isProduction } = require('../../config/keys');
 
+const validateRegisterInput = require('../../validations/register');
+const validateLoginInput = require('../../validations/login');
+
+const { isProduction } = require('../../config/keys');
 
 router.get('/current', restoreUser, (req, res) => {
   if (!isProduction) {
@@ -32,7 +35,7 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.post('/register', async (req, res, next) => {
+router.post('/register', validateRegisterInput, async (req, res, next) => {
   // Check to make sure no one has already registered with the proposed email or
   // username.
   const user = await User.findOne({
@@ -77,7 +80,7 @@ router.post('/register', async (req, res, next) => {
 });
 
 // POST /api/users/login
-router.post('/login', async (req, res, next) => {
+router.post('/login', validateLoginInput, async (req, res, next) => {
   passport.authenticate('local', async function(err, user) {
     if (err) return next(err);
     if (!user) {
